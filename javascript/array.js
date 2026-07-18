054a6cc6f8d1bcc70a9951dad0f98fa25038f83d
Parv shah <parvs247@gmail.com>
2026-07-18 20:32:15 +0530

    Add array.js file and update index.html to include it

---
+++ javascript/array.js
@@ -0,0 +1,6 @@
+//array is the collection of same datatypes stored in a contiguous memory location. In js it is mutable
+
+let marks=[97,67,87,45,34];
+
+console.log(marks);
+console.log(marks.length);
