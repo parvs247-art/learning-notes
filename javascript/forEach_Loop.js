f05686609428639bfdf711fb182ab2c9b44c9cea
Parv shah <parvs247@gmail.com>
2026-08-27 22:58:06 +0530

    Add forEach_Loop.js to demonstrate array iteration and squaring values

---
+++ javascript/forEach_Loop.js
@@ -0,0 +1,15 @@
+// let arr=["pune","mumbai","delhi","banglore"];
+
+// arr.forEach((val)=>{
+//     console.log(val);
+// });
+
+
+
+//print the square of the numbers in the array
+let arr=[2,3,4,5,6];
+
+arr.forEach((val)=>{
+    console.log(val*val); // 4,9,16,25,36
+
+});
