67fcdd51722cd70e3c8005d6e0453db9a80205ec
Parv shah <parvs247@gmail.com>
2026-09-09 23:41:24 +0530

    Add C++ program for printing patterns

---
+++ Cpp/patterns.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print1(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<< "*";
+        }
+        cout<<endl;
+    }
+}
+
+void print2(int n){
+    for (int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<< "*";
+        }
+        cout<<endl;
+    }
+}
+
+void print3(int n){
+    for(int i=1;i<n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+void print4(int n){
+    for(int i=n;i>0;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cout<<"enter the number of rows: ";
+     cin>>n;
+     print4(n);
+    return 0;
+}
