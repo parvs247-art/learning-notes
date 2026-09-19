d0a01f3146a845bbd67c3121a32df940a94ef0ec
Parv shah <parvs247@gmail.com>
2026-09-19 23:13:48 +0530

    Add pattern printing functions and update main to call print7

--- Cpp/patterns.cpp
+++ Cpp/patterns.cpp
@@ -1,5 +1,13 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h>                           
 using namespace std;
+/*
+steps to print a pattern
+step1: for the outer loops count the no. of lines
+step2: for the inner loop focus on the columns and connect them somehow to the rows 
+*/
+
+
+
 
 void print1(int n){
     for(int i=0;i<n;i++){
@@ -37,10 +45,46 @@ void print4(int n){
     }
 }
 
+void print5(int n){
+    for (int i=1;i<n;i++){
+        for(int j=1;j<=i;j++){
+            cout<< i << " ";
+        }
+        cout<<endl;
+    }
+}
+
+void print6(int n){
+    for (int i=1;i<n;i++){
+        for(int j=1;j<=i;j++){
+            cout<< j << " ";
+        }
+        cout<<endl;
+    }
+}
+
+void print7(int n){
+    for (int i=0;i<n;i++){
+       // spaces
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        // stars
+        for(int j=0;j<2*i+1;j++){
+            cout<< "*";
+        }
+        // spaces
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     int n;
     cout<<"enter the number of rows: ";
      cin>>n;
-     print4(n);
+     print7(n);
     return 0;
 }
